c901595d6d41da444ae45885c2330e7b9bb077fd
Blallo <blallo@autistici.org>
2025-01-21 23:04:22 +0100

    Update cmp config

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -1,32 +1,56 @@
 local cmp = require('cmp')
 
 cmp.setup({
+    completion = {
+        keyword_length = 0,
+        keyword_pattern = "*",
+    },
     snippet = function(args)
-      vim.fn["vsnip#anonymous"](args.body)
+        vim.fn["vsnip#anonymous"](args.body)
     end,
     window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
-      ['<C-Down>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-Up>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-Down>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-Up>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
     }),
     sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
-      { name = 'vsnip' },
+        { name = 'nvim_lsp', keyword_length = 0, keyword_pattern = ".*", },
+        { name = 'vsnip',    keyword_length = 1 },
     }, {
-      { name = 'buffer' },
-    })
+        { name = 'buffer', keyword_length = 0 },
+        { name = 'path',   keyword_length = 0, keyword_pattern = "\\.?/", },
+    }),
+    view = { entries = "custom" },
 })
+
+cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    }
+})
+
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        { name = 'cmdline' }
+    }),
+    matching = { disallow_symbol_nonprefix_matching = false }
+})
+
 
 local servers = require('lspconfig')
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 for _, server in ipairs(servers) do
-  require('lspconfig')[server].setup {
-    capabilities = capabilities
-  }
+    require('lspconfig')[server].setup {
+        capabilities = capabilities
+    }
 end
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,8 +1,12 @@
 return {
     "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
+    event = { "InsertEnter", "CmdlineEnter" },
     dependencies = {
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/cmp-vsnip",
+        "hrsh7th/vim-vsnip",
     },
 }
