d55488e52ea69fe99309b0130d8c9fa956eeea95
Blallo <blallo@autistici.org>
2025-04-04 15:49:38 +0200

    Fix lsp-based completion

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -1,5 +1,9 @@
 local cmp = require('cmp')
 
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
+
+local select_opts = { behavior = cmp.SelectBehavior.Select }
+
 cmp.setup({
     completion = {
         keyword_length = 1,
@@ -18,15 +22,34 @@ cmp.setup({
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            local col = vim.fn.col('.') - 1
+
+            if cmp.visible() then
+                cmp.select_next_item(select_opts)
+            elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+                fallback()
+            else
+                cmp.complete()
+            end
+        end, { 'i', 's' }),
+
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item(select_opts)
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
     }),
     sources = cmp.config.sources({
-        { name = 'nvim_lsp', keyword_length = 0 },
-        { name = 'vsnip',    keyword_length = 1 },
-    }, {
-        { name = 'buffer', keyword_length = 3 },
-        { name = 'path',   keyword_length = 0, keyword_pattern = "\\.?/", },
+        -- { { name = 'nvim_lsp' }, { name = 'vsnip' } }, { { name = 'buffer' }, { name = 'path' } }
+        { name = 'nvim_lsp', score = 3 },
+        { name = 'vsnip',    score = 3 },
+        { name = 'buffer',   score = 1 },
+        { name = 'path',     score = 1, keyword_pattern = "\\.?/", },
     }),
-    view = { entries = "custom" },
+    --view = { entries = "custom" },
 })
 
 cmp.setup.cmdline({ '/', '?' }, {
